00ddb45cbda6860ec97c4bb7a3154581b9c6a71b
Florian-Elz <email@florian-elze.de>
2025-07-03 09:16:52 +0200

    1. Version

---
+++ hallo.py
@@ -0,0 +1,15 @@
+Zahl1=int(input("1.Zahl: "))
+Zahl2=int(input("2.Zahl: "))
+
+Rechenzeichen=input("Addieren? (+), Subtrahieren? (-), Multiplizieren? (*), Dividieren? (/): ")
+Ergebnis=0
+if Rechenzeichen=="+":
+    Ergebnis=Zahl1+Zahl2
+if Rechenzeichen=="-":
+    Ergebnis=Zahl1-Zahl2
+if Rechenzeichen=="*":
+    Ergebnis=Zahl1*Zahl2
+if Rechenzeichen=="/":
+    Ergebnis=Zahl1/Zahl2
+print("Das Ergebnis ist:", Ergebnis)
+
