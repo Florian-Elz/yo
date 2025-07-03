9e8e02df26c0a3870d0998cc9f8b2afa2baec114
Florian-Elz <email@florian-elze.de>
2025-07-03 14:47:32 +0200

    Fixed that only valid answers could be given

--- hallo2.py
+++ hallo2.py
@@ -1,24 +1,31 @@
 Frage =False
 a=0
 List = [] 
+NochEine = False
 
 while Frage == False:
     
     a += 1 
-    Zahl1=int(input("Zahl" + str(a) + ": "))
-    
-    
+    Zahl1=int(input("Zahl" + str(a) + ": "))    
     item = Zahl1
     List.append(item)
+
+    while NochEine == False:
+        Frage1=str(input("Noch eine?: "))
+        if Frage1 =="nein":
+            Frage = True
+            NochEine = True
+        elif Frage1 == "ja":
+            Frage = False 
+            NochEine = True
+        else:
+            print("Gib bitte eine Gültige antwort ein! (Ja, Nein)")
+            NochEine = False
+    NochEine = False
     
-    Frage1=str(input("noch eine?: "))
     
-    if Frage1 =="nein":
-        Frage = True
-        
-    else:
-        Frage = False    
-     
+            
+    
 print(List)
 Rechenzeichen=input("Addieren? (+), Subtrahieren? (-), Multiplizieren? (*), Dividieren? (/), : ")
 Ergebnis=0
@@ -38,6 +45,7 @@ if Rechenzeichen=="/":
     for x in List[1:]:
         Ergebnis /= x 
 
+            
 
     
 print("Das Ergebnis ist:", Ergebnis)
